f3e203273994f76828d634c249995344dc5ec452
Volobueva-Yuliya <volobueva.juli@gmail.com>
2023-03-28 20:53:28 +0400

    fix

--- dcstit/datasets/image_list_dataset.py
+++ dcstit/datasets/image_list_dataset.py
@@ -3,8 +3,10 @@ from PIL import Image
 
 class ImageListDataset(Dataset):
 
-    def __init__(self, images, source_transform=None, names=None):
+    def __init__(self, images, quads, image_size, source_transform=None, names=None):
         self.images = images
+        self.quads = quads
+        self.image_size = image_size
         self.source_transform = source_transform
         self.names = names
 
@@ -13,8 +15,9 @@ class ImageListDataset(Dataset):
 
     def __getitem__(self, index):
         image = self.images[index]
+        quad = self.quads[index]
         name = self.names[index] if self.names is not None else str(index)
-        from_im = Image.open(image)
+        from_im = crop_image(image, self.image_size, quad.copy())
 
         if self.source_transform:
             from_im = self.source_transform(from_im)
